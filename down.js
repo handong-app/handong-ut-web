b8bc2b02d1433ae180ce564d2dbf16cef18f9dd3
healimfx <22000770@handong.edu>
2021-11-10 04:31:00 +0900

    Create down.js

---
+++ down.js
@@ -0,0 +1,16 @@
+var height = document.getElementById('main_page').offsetHeight;
+const saE = document.querySelectorAll('.download');
+
+const Func = function() {
+  for (const element of saE) {
+    console.log(height, window.scrollY)
+    if (!element.classList.contains('show_d')) {
+      if (window.scrollY > height/5) {
+        element.classList.add('show_d');    
+      }
+    }
+  }
+}
+
+window.addEventListener('load', Func);
+window.addEventListener('scroll', Func);
